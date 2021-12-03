f4280ce4c7ff66c3094a9d3687efd32666b49fa2
Vtga.21 <glebvt51@gmail.com>
2021-12-03 12:14:41 +0300

    les 3.12

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-a=[]
-for i in range(0,10):
-    s=int(input())
-    a.append(s)
-print(a)
+#переменные для ввода
+"""
+3
+f a 44
+g r 56
+
+d a 90
+f g 78
+p o 67
+
+t y 54
+"""
+def fill_set():
+    _set = dict()
+    str = input()
+    while str!='':
+        _str = str.split()
+        _set[_str[0] +" "+ _str[1]] = int(_str[2])
+        str = input()
+    return _set
+
+count = int(input())
+list_dict = [0] * count
+for i in range (count):
+    list_dict[i]=fill_set()
+
+#_m=fill_set()
+#_p=fill_set()
+#_i=fill_set()
+
+print(list_dict)
